2da05f673f5f020861ada6cd255ca56c433f0a2d
duclam227 <duclam227@gmail.com>
2021-01-10 20:24:02 +0700

    added forms

---
+++ ConcurrencyControl/ConcurrencyControl/Deadlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConcurrencyControl
+{
+    public partial class Deadlock : Form
+    {
+        public Deadlock()
+        {
+            InitializeComponent();
+        }
+    }
+}
